3514a54c8c3b5093f37e3cc5b72d311dd811641d
Joel Margosian <margosij@gmail.com>
2020-01-02 19:20:45 -0500

    code moved to correct locations. buttons are double rendering, trying to fix that

--- react-clicky-game/src/components/GameButtons/index.js
+++ react-clicky-game/src/components/GameButtons/index.js
@@ -1,32 +1,36 @@
 import React from "react";
 import "./style.css";
+import buttons from "../../buttons.json"
+
+const GameButton = (props) => {
+  return (
+  <div className="card">
+    <button onClick = {props.changeScore} className="img-container">
+      <img alt={props.id} src={props.image} />
+    </button>
+  </div>
+  )
+}
 
 //create an on click function to handle each click
-class App extends React.Component{
+class GameButtons extends React.Component{
   state = {
     buttons
   }
 
 //will need to remove the "function" when uncommenting everything else
-  GameButtons(props) {
-
-    <div className="card">
-      <button onClick = {props.changeScore} className="img-container">
-        <img alt={props.id} src={props.image} />
-      </button>
-    </div>
-    
-  }
   render () {
     return(
+      <>
       {this.state.buttons.map(button => (
-        <this.GameButtons
+        <GameButton
         id={button.id}
-          image={button.image}
+        image={button.image}
           />
       ))}
-    )}
-    
+      </>
+    )
+  }    
 }
 
 export default GameButtons;
