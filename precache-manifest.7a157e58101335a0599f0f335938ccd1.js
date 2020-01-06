34fec22a33ecaad35bf9454f8687ccb2e76bb899
Joel Margosian <margosij@gmail.com>
2020-01-06 18:10:18 -0500

    Updates

---
+++ precache-manifest.7a157e58101335a0599f0f335938ccd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dabdf377e3ef009233b4699391784f",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "b78031a34f3a9bfd0908",
+    "url": "/react-clicky-game/static/css/main.d17a2ccd.chunk.css"
+  },
+  {
+    "revision": "4c0332882f7342244fd3",
+    "url": "/react-clicky-game/static/js/2.cfe38eb6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clicky-game/static/js/2.cfe38eb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b78031a34f3a9bfd0908",
+    "url": "/react-clicky-game/static/js/main.64e15667.chunk.js"
+  },
+  {
+    "revision": "d65c7077d7efcf6a2a97",
+    "url": "/react-clicky-game/static/js/runtime-main.2ae528d8.js"
+  }
+]);
